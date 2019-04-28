50bc59600da0ba29e4dca3f82f4d3f461e92c290
timothy-tianmu <tianmu_19970924@163.com>
2019-04-29 02:35:49 +0800

    optimize experience

--- page/project-list.js
+++ page/project-list.js
@@ -50,20 +50,29 @@ class ProjectList {
       this.parentElement.insertAdjacentHTML("beforeend", `
         <li data-projectid="${p.id}" class="project-item">
           <img class="project-thumb" src="${p.dataURL}"> 
-          <div class="project-operations">
-            <span>下载</span>
-            <span>删除</span>
-          </div>
         </li> 
       `)
     })
     this.parentElement.addEventListener("click", (ev) => {
       var t = ev.target
-      if (t.nodeName.toLowerCase() === "li") {
-        var pid = t.dataset.projectid
-        var project = this.projects.get(pid)
-        window.eventbus.emit("load_project", project)
+      while (!t.classList.contains("project-item")) {
+        if (t.classList.contains("project-list")) {
+          return
+        }
+        t = t.parentElement
       }
+      if (!t.classList.contains("active")) {
+        t.classList.add("active")
+        var items = this.parentElement.querySelectorAll(".project-item")
+        for (var i of items) {
+          if (i !== t && i.classList.contains("active")) {
+            i.classList.remove("active")
+          }
+        }
+      }
+      var pid = t.dataset.projectid
+      var project = this.projects.get(pid)
+      window.eventbus.emit("load_project", project)
     })
   }
 

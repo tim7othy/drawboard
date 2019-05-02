4cc17fd7e7cc75a4b72e195d25e413bc65484795
timothy-tianmu <tianmu_19970924@163.com>
2019-05-02 16:01:24 +0800

    refactor code

--- page/board-config.js
+++ page/board-config.js
@@ -4,23 +4,9 @@ class BoardConfig {
   }
 
   bindEvents() {
-    // var lineWidthInput = document.getElementById("board_line_width")
-    // var colorInput = document.getElementById("board_color")
-    // var textWidthInput = document.getElementById("board_text_width")
-    // var textHeightInput = document.getElementById("board_text_height")
-    // var fontSizeInput = document.getElementById("board_font-size")
-    // var eraserSizeInput = document.getElementById("board_eraser_size")
-    // var eraserShapeInput = document.getElementById("board_eraser_shape")
     var detailPane = document.querySelector(".board-details-wrapper")
-    detailPane.addEventListener("input", (ev) => {
-      var t = ev.target
-      while(!t.id || !t.id.startsWith("board_")) {
-        if (t.classList.contains("board-detail-wrapper")) {
-          return
-        }
-        t = t.parentElement
-      }
-      window.eventbus.emit(t.id + "_change", t.value)
+    delegate(detailPane, "input", ".config-tool", (ev, el) => {
+      window.eventbus.emit(el.id + "_change", el.value)
     })
   }
 }
--- page/tool-pane.js
+++ page/tool-pane.js
@@ -6,16 +6,9 @@ class ToolPane {
 
   bindEvents() {
     // 在工具条Dom上代理各个工具按钮的点击事件
-    this.el.addEventListener("click", (ev) => {
-      var elem = ev.target
-      while (elem.classList[0] != "tool") {
-        if (elem.id === "toolPane") {
-          return
-        }
-        elem = elem.parentElement
-      }
+    delegate(this.el, "click", ".tool", (ev, el) => {
       // 根据点击的按钮包含的工具的类型，切换当前使用的工具
-      var toolType = elem.dataset.name
+      var toolType = el.dataset.name
       window.eventbus.emit("switch-tool", toolType)
     })
   }
